6fc808d251e0c6de87c87e6f10c1205dd68e40dc
ginasonia99 <sonia.ghina99@gmail.com>
2020-04-15 09:50:43 +0800

    Percabangan Switch

--- src/percabangan/PercabanganSwitch.java
+++ src/percabangan/PercabanganSwitch.java
@@ -1,24 +1,30 @@
 package percabangan;
 
 public class PercabanganSwitch {
-        public static void main(String[] args) {
-            int jalanJalan=3;
-            switch(jalanJalan){
-                case 1:
-                    System.out.println("urang lapas");
-                    break;
-                case 2:
-                    System.out.println("urang acayan");
-                    break;
-                case 3:
-                    System.out.println("ubat nyamuk");
-                    break;
-                case 4:
-                    System.out.println("dubil dit");
-                    break;
-                default:
-                    System.out.println("mahluk halus");
-                    break;
+    public static void main(String[] args) {
+        char nilai='D';
+        switch(nilai){
+            case 'A':
+                System.out.println("Mhs    : \"Terima kasih pak\"");
+                System.out.println("Dosen  : \"Selamat ya !!\"");
+                break;
+            case 'B':
+                System.out.println("Mhs    : \"Kenapa ulun kada dapat A pak\"");
+                System.out.println("Dosen  : \"!@#$%\"");
+                break;
+            case 'C':
+                System.out.println("Mhs    : \"Ulun turun pul Pak ai, tugas pul jua\"");
+                System.out.println("Dosen  : \"Tapi bisa lah menjawab ujian?\"");
+                System.out.println("Mhs    : \"Hihihi..\"");
+                break;
+            default:
+                System.out.println("Mhs    : \"Ulun turun pul Pak ai, tugas pul jua\"");
+                System.out.println("Dosen  : \"Bujur jua kah?\"");
+                System.out.println("Dosen  : \"*meriksa berkas\"");
+                System.out.println("Dosen  : \"*menceleng\"");
+                System.out.println("Mhs    : \"Kabur..\"");
+                break;
         }
     }
 }
+
